1cc27873340b6f84776e37e2d6dbbde5df712312
karina <karinagutmann@gmail.com>
2020-08-03 20:00:08 +0300

    finish deployment

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     cookieName: 'ins_user',
     secret: '654987cookie7',
-    dbUrl: 'mongodb://localhost:27017/instagram',
-    port: 4000
+    dbUrl: process.env.MONGO_URL,
+    port: process.env.PORT
 };

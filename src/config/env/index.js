b9a8f605ce8347c9634e6ce68907c2a7ffc5f076
karina <karinagutmann@gmail.com>
2020-08-03 20:06:38 +0300

    finish deployment

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -1,4 +1,9 @@
 const development = require('./development');
 const production = require('./production');
 
-module.exports = development;
+let env = development;
+if (process.env.NODE_ENV === 'production') {
+    env = production;
+}
+
+module.exports = env;
